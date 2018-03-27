894a5cc57852864632768f8c0c1509abbf5e3e7c
REAOrlando <Chris@REAOrlando.com>
2018-03-26 23:22:16 -0400

    Initial Commit

---
+++ DBPR Upload Tool/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DBPR Upload Tool
+//
+//  Created by CHRISTOPHER ALBANESE on 3/26/18.
+//  Copyright © 2018 CHRISTOPHER ALBANESE. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DBPR Upload Tool/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DBPR Upload Tool
+//
+//  Created by CHRISTOPHER ALBANESE on 3/26/18.
+//  Copyright © 2018 CHRISTOPHER ALBANESE. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
